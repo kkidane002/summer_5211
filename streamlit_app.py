f65123ec283fbc7057cd8344fc45088008b43ead
kkidane002 <kkidane@uncc.edu>
2024-06-20 19:57:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,16 @@ st.line_chart(sales_by_month, y="Sales")
 
 st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
+
+# Extract unique categories from the 'Category' column
+categories = df['Category'].unique()
+
+# Create a selectbox with the unique categories
+option = st.selectbox(
+    "Select a category:",
+    categories
+)
+st.write("You selected:", option)
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
