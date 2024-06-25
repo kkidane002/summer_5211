3cfc3eed39c1cda6671eb43a65634ac4bd9c52ad
kkidane002 <kkidane@uncc.edu>
2024-06-25 13:27:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ st.dataframe(sales_by_month)
 # Here the grouped months are the index and automatically used for the x axis
 st.line_chart(sales_by_month, y="Sales")
 
-st.write("## Your additions")
-st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
+st.write("Addition Below")
 
+st.write('### (1)")
 # Extract unique categories from the 'Category' column
 categories = df['Category'].unique()
 
